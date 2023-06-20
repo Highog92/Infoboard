6d9f86e67fd2ec5f36533391c62bf2bb7750298a
Highog92 <Madsserritslev@gmail.com>
2023-06-20 14:18:21 +0200

    Update Bustimes.js

--- Assets/Javascript/Bustimes.js
+++ Assets/Javascript/Bustimes.js
@@ -24,44 +24,6 @@ function getBuses() {
         .finally(() => {
             //When all is set and done
 
-        //     let removeRoadName0 = busarrivals.MultiDepartureBoard.Departure[0].stop
-        //         .replace(/\s*\([^)]*\)/, ''); // remove anything inside parentheses
-        //     let removeRoadName1 = busarrivals.MultiDepartureBoard.Departure[1].stop
-        //         .replace(/\s*\([^)]*\)/, '');
-        //     let removeRoadName2 = busarrivals.MultiDepartureBoard.Departure[2].stop
-        //         .replace(/\s*\([^)]*\)/, '');
-        //     let removeRoadName3 = busarrivals.MultiDepartureBoard.Departure[3].stop
-        //         .replace(/\s*\([^)]*\)/, '');
-        //     let removeRoadName4 = busarrivals.MultiDepartureBoard.Departure[4].stop
-        //         .replace(/\s*\([^)]*\)/, '');
-
-        //     document.querySelector('#bus-times').innerHTML = `
-        //      <div class="allBuses">
-        //         <p class="allBusesLine">${busarrivals.MultiDepartureBoard.Departure[0].line}</p>
-        //         <p>${removeRoadName0.toUpperCase()}</p>
-        //         <p>${busarrivals.MultiDepartureBoard.Departure[0].time}</p>
-        //     </div>
-        //     <div class="allBuses">
-        //         <p class="allBusesLine">${busarrivals.MultiDepartureBoard.Departure[1].line}</p>
-        //         <p>${removeRoadName1.toUpperCase()}</p>
-        //         <p>${busarrivals.MultiDepartureBoard.Departure[1].time}</p>
-        //     </div>
-        //     <div class="allBuses">
-        //         <p class="allBusesLine">${busarrivals.MultiDepartureBoard.Departure[2].line}</p>
-        //         <p>${removeRoadName2.toUpperCase()}</p>
-        //         <p>${busarrivals.MultiDepartureBoard.Departure[2].time}</p>
-        //     </div>
-        //     <div class="allBuses">
-        //         <p class="allBusesLine">${busarrivals.MultiDepartureBoard.Departure[3].line}</p>
-        //         <p>${removeRoadName3.toUpperCase()}</p>
-        //         <p>${busarrivals.MultiDepartureBoard.Departure[3].time}</p>
-        //     </div>
-        //     <div class="allBuses">
-        //         <p class="allBusesLine">${busarrivals.MultiDepartureBoard.Departure[4].line}</p>
-        //         <p>${removeRoadName4.toUpperCase()}</p>
-        //         <p>${busarrivals.MultiDepartureBoard.Departure[4].time}</p>
-        //     </div>
-        //  `
             setTimeout(() => {
                 getBuses()
             }, 300000);
